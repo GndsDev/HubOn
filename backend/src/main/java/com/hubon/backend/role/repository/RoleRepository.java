7f05fdb2d0ba61808e219b60cb7068136ea08544
GndsDev <gabrielnunesdossantoss@gmail.com>
2026-06-08 09:46:52 -0300

    feat: adicionar repositórios de usuário e perfil

---
+++ backend/src/main/java/com/hubon/backend/role/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.hubon.backend.role.repository;
+
+import com.hubon.backend.role.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+
+    boolean existsByName(String name);
+}
